e2ffa295fbc5181f7fc7911b671aa090dcabc779
jaxpro-code <jax.bradley.pro@gmail.com>
2026-06-14 17:05:45 -0400

    dealership service formatting

---
+++ src/main/java/com/example/vehicle_dealership/services/DealershipService.java
@@ -0,0 +1,19 @@
+package com.example.vehicle_dealership.services;
+
+import com.example.vehicle_dealership.repositories.DealershipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DealershipService {
+    @Autowired
+    private DealershipRepository dealershipRepository;
+
+    //create
+    //read
+    //update
+    //delete
+    //by id
+    //by name
+
+}
