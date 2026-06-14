4b3b305a1e3996b7a94366151eea002958e4fabc
jaxpro-code <jax.bradley.pro@gmail.com>
2026-06-14 15:33:27 -0400

    vehicle repo formatting

--- src/main/java/com/example/vehicle_dealership/repositories/VehicleRepository.java
+++ src/main/java/com/example/vehicle_dealership/repositories/VehicleRepository.java
@@ -4,7 +4,33 @@ import com.example.vehicle_dealership.entities.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    //provided crud queries are based on the object as a whole, we have to write the ones that break up the object
+    //provided crud, create a vehicle, update  a vehicle, find by id, find all, delete by id delete vehicle, check existace
+
+    //by price
+    List<Vehicle> findByPriceBetween(Double min, Double max);
+
+    //by make
+    List<Vehicle> findByMakeIgnoreCase(String make);
+
+    //by model
+    List<Vehicle> findByModelIgnoreCase(String model);
+
+    //by year
+    List<Vehicle> findByYearBetween(Integer min, Integer max);
+
+    //by color
+    List<Vehicle> findByColorIgnoreCase(String color);
+
+    //by miles
+    List<Vehicle> findByMilesBetween(Integer min, Integer max);
+
+    //by type
+    List<Vehicle> findByTypeIgnoreCase(String type);
+
 
 }
