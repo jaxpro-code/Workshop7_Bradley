b84b4193c867a5caf04a847f4ef13481e940820e
jaxpro-code <jax.bradley.pro@gmail.com>
2026-06-14 11:27:44 -0400

    Initial commit

---
+++ src/test/java/com/example/vehicle_dealership/VehicleDealershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_dealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleDealershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
